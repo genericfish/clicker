2544b0df35b7c52949c24de0d8d727d5a92e103f
barnden <brandon.nguyen1129@gmail.com>
2020-12-11 18:42:26 -0600

    add golden khoi and let game run in background

--- assets/khoima.js
+++ assets/khoima.js
@@ -76,6 +76,79 @@ let game = (() => {
             setTimeout(() => {
                 document.body.removeChild(container)
             }, 1000)
+        },
+        create_row: (tower, number) => {
+            let row = document.createElement("div")
+
+            row.setAttribute(
+                "style",
+                `background: url(assets/${tower}.png);` +
+                "background-size: 45px 45px;" +
+                "height: 45px;" +
+                `width: ${number * 45}px;` +
+                "margin: 0 auto;"
+            )
+
+            return row
+        },
+        create_shop: () => {
+            for (tower in towers) {
+                let listing = document.createElement("li")
+                let link = document.createElement("a")
+
+                let eventHandler = (tower) => {
+                    return () => {
+                        shop.stats.header.innerHTML = towers[tower].name
+                        shop.stats.rate.innerHTML = towers[tower].base_rate
+                        shop.stats.click.innerHTML = towers[tower].base_click
+                        shop.active = tower
+
+                        shop.stats.owned.innerHTML = game.towers[shop.active][0]
+                        shop.stats.producing.innerHTML = nice_format(game.towers[shop.active][1].toFixed(2))
+
+                        update_costs()
+                    }
+                }
+
+                link.addEventListener("click", eventHandler(tower))
+
+                if (tower == "autoclicker") link.click()
+
+                link.href = "#"
+                link.innerHTML = towers[tower].name
+                listing.appendChild(link)
+                shop.listings.appendChild(listing)
+            }
+
+            shop.stats.radio[0].click()
+        },
+        update_buildings: () => {
+            windows[2].innerHTML = ""
+
+            for (tower in towers) {
+                let count = game.towers[tower][0]
+                if (!count) continue
+
+                let fieldset = document.createElement("fieldset")
+                let legend = document.createElement("legend")
+                let container = document.createElement("div")
+
+                legend.innerHTML = towers[tower].name + " x " + count
+
+                container.classList.add("row-container")
+
+                for (let x = 0; x < Math.floor(count / 10); x++)
+                    container.appendChild(graphics.create_row(tower, 10))
+
+                if (count % 10 > 0)
+                    container.appendChild(graphics.create_row(tower, count % 10))
+
+                fieldset.setAttribute("style", "border: 1px solid black;")
+                fieldset.appendChild(legend)
+                fieldset.appendChild(container)
+
+                windows[2].appendChild(fieldset)
+            }
         }
     }
 
@@ -92,7 +165,7 @@ let game = (() => {
             base_cost: 175,
             cost_multiplier: 1.0135,
             base_rate: 2,
-            base_click: 0
+            base_click: 0,
         },
         coomfactory: {
             name: "coom factory",
@@ -105,7 +178,7 @@ let game = (() => {
             name: "dog farm",
             base_cost: 10000,
             cost_multiplier: 1.014,
-            base_rate: 50,
+            base_rate: 60,
             base_click: 0,
         },
     }
@@ -153,20 +226,6 @@ let game = (() => {
         display.total.innerHTML = nice_format(Math.round(game.gamergoo))
     }
 
-    let last_click = Date.now()
-
-    display.button.addEventListener("mousedown", e => {
-        let now = Date.now()
-        if (now - last_click <= 77) return
-
-        let amount = game.modifiers.click[0] + 1
-        last_click = now
-
-        add_goo(amount)
-        graphics.floater(amount, e.clientX, e.clientY)
-        graphics.bounce()
-    })
-
     let shop = {
         listings: document.getElementById("listings"),
         active: undefined,
@@ -192,82 +251,6 @@ let game = (() => {
         }
     }
 
-    function create_shop() {
-        for (tower in towers) {
-            let listing = document.createElement("li")
-            let link = document.createElement("a")
-
-            let eventHandler = (tower) => {
-                return () => {
-                    shop.stats.header.innerHTML = towers[tower].name
-                    shop.stats.rate.innerHTML = towers[tower].base_rate
-                    shop.stats.click.innerHTML = towers[tower].base_click
-                    shop.active = tower
-
-                    shop.stats.owned.innerHTML = game.towers[shop.active][0]
-                    shop.stats.producing.innerHTML = nice_format(game.towers[shop.active][1].toFixed(2))
-
-                    update_costs()
-                }
-            }
-
-            link.addEventListener("click", eventHandler(tower))
-
-            if (tower == "autoclicker") link.click()
-
-            link.href = "#"
-            link.innerHTML = towers[tower].name
-            listing.appendChild(link)
-            shop.listings.appendChild(listing)
-        }
-
-        shop.stats.radio[0].click()
-    }
-
-    function create_row(tower, number) {
-        let row = document.createElement("div")
-
-        row.setAttribute(
-            "style",
-            `background: url(assets/${tower}.png);` +
-            "background-size: 45px 45px;" +
-            "height: 45px;" +
-            `width: ${number * 45}px;` +
-            "margin: 0 auto;"
-        )
-
-        return row
-    }
-
-    function update_buildings() {
-        windows[2].innerHTML = ""
-
-        for (tower in towers) {
-            let count = game.towers[tower][0]
-            if (!count) continue
-
-            let fieldset = document.createElement("fieldset")
-            let legend = document.createElement("legend")
-            let container = document.createElement("div")
-
-            legend.innerHTML = towers[tower].name + " x " + count
-
-            container.classList.add("row-container")
-
-            for (let x = 0; x < Math.floor(count / 10); x++)
-                container.appendChild(create_row(tower, 10))
-
-            if (count % 10 > 0)
-                container.appendChild(create_row(tower, count % 10))
-
-            fieldset.setAttribute("style", "border: 1px solid black;")
-            fieldset.appendChild(legend)
-            fieldset.appendChild(container)
-
-            windows[2].appendChild(fieldset)
-        }
-    }
-
     function setup() {
         if (window.localStorage["gamestate"] == undefined)
             save()
@@ -285,8 +268,30 @@ let game = (() => {
                 else
                     game[key] = game_load[key]
 
-        create_shop()
-        update_buildings()
+        graphics.create_shop()
+        graphics.update_buildings()
+
+        let clicker = (() => {
+            let last_click = Date.now()
+
+            display.button.addEventListener("mousedown", e => {
+                let now = Date.now()
+                if (now - last_click <= 77) return
+
+                let amount = game.modifiers.click[0] + 1
+
+                if (goldenkhoi_active)
+                    amount += Math.max(game.rate * .15, 5)
+
+                amount += game.rate * .01
+
+                last_click = now
+
+                add_goo(amount)
+                graphics.floater(Math.round(amount * 100) / 100, e.clientX, e.clientY)
+                graphics.bounce()
+            })
+        })()
     }
 
     function geometric_sum(base, ratio, exp) {
@@ -344,36 +349,103 @@ let game = (() => {
         display.rate.innerHTML = nice_format(game.rate.toFixed(2)) + " gamergoo per second"
     }
 
+    // Golden appears every 1 to 6 minutes
+    let goldenkhoi = Math.ceil((Math.random() * 18000) + 300)
+    let goldenkhoi_active = false
+
+    function make_khoi() {
+        let khoi = document.createElement("div")
+        let removed = false
+
+        khoi.setAttribute("id", "goldenkhoi")
+        khoi.setAttribute("style",
+            `top:${Math.ceil(Math.random() * 70) + 15}%;` +
+            `left:${Math.ceil(Math.random() * 70) + 15}%;`
+        )
+
+        khoi.addEventListener("click", e => {
+            goldenkhoi_active = true
+            document.body.removeChild(khoi)
+
+            for (let x = 1; x <= 3; x++) windows[x].parentElement.classList.add("khoi")
+
+            setTimeout(() => {
+                goldenkhoi_active = false
+                removed = true
+                for (let x = 1; x <= 3; x++) windows[x].parentElement.classList.remove("khoi")
+            }, 10000)
+        })
+
+        setTimeout(() => {
+            if (!removed) document.body.removeChild(khoi)
+        }, 45000)
+
+        document.body.appendChild(khoi)
+    }
+
+    let last_click = Date.now()
+
+    document.addEventListener("click", (e) => {
+        last_click = Date.now()
+    })
+
     let loop = (() => {
         let last_frame = Date.now()
         let frames = 0
+        let last_updates = [0,0,0]
 
         return () => {
+            let focused = !document["hidden"]
             let now = Date.now()
 
             // Approximately 100 FPS
-            if (now - last_frame >= 10) frames++
-    
-            // Every other frame (20ms) add 1/50th of goo
-            if (frames % 2 == 0)
-                add_goo(game.rate / 50)
-    
-            // Every 25th frame (250ms) update shop buy button
-            if (frames % 25 == 0) {
+            if (now - last_frame >= 10)
+                frames += Math.round((now - last_frame) / 10)
+
+            // Every other frame (~20ms) add 1/50th of goo
+            if (frames - last_updates[0] >= 2) {
+                last_updates[0] = frames
+
+                let goo = undefined
+
+                if (!focused) {
+                    goo = game.rate
+                    document.title = nice_format(Math.round(game.gamergoo)) + " gamergoo | Khoima Clicker"
+                    save()
+                } else goo = game.rate / 50
+
+                if (now - last_click > 300000) goo *= .15
+
+                add_goo(goo)
+            }
+
+            // Every 25th frame (~250ms) update shop buy button
+            if (frames - last_updates[1] >= 25) {
                 if (shop.active_cost > game.gamergoo)
                     shop.stats.buy.setAttribute("disabled", "disabled")
                 else
                     shop.stats.buy.removeAttribute("disabled")
+
+                document.title = nice_format(Math.round(game.gamergoo)) + " gamergoo | Khoima Clicker"
+                last_updates[1] = frames
             }
-    
-            // Save game every second (100 frames)
-            if (frames % 100 == 0)
+
+            // Save game every second (~100 frames)
+            if (frames - last_updates[2] >= 25) {
                 save()
-    
-            if (running)
-                window.requestAnimationFrame(loop)
+                last_updates[2] = frames
+            }
+
+            if (frames == goldenkhoi) {
+                goldenkhoi = frames + Math.ceil((Math.random() * 30000) + 6000)
+                make_khoi()
+            }
+
+            last_frame = now
         }
-    })()
+    })
+
+    setInterval(loop(), 10)
 
     // exports
     return {
@@ -416,7 +488,7 @@ let game = (() => {
             if (update) {
                 update_costs()
                 update_rates()
-                update_buildings()
+                graphics.update_buildings()
 
                 shop.stats.owned.innerHTML = game.towers[shop.active][0]
                 shop.stats.producing.innerHTML = nice_format(game.towers[shop.active][1].toFixed(2))
@@ -425,8 +497,10 @@ let game = (() => {
             }
         },
         stats: () => {
-            console.log(game.gamergoo)
-            console.log(game.gamergoo_history)
+            console.log(
+                "gamergoo: " + game.gamergoo + '\n' +
+                "total gamergoo: " + game.gamergoo_history + '\n'
+            )
         },
         wipe: () => {
             running = false
@@ -435,9 +509,7 @@ let game = (() => {
                 window.location.reload()
             }, 100)
         },
-        hack: (amt) => {
-            add_goo(amt, false)
-        }
+        hack: (amt) => { add_goo(amt, false) }
     }
 })()
 
