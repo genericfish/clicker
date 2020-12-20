dd5a2f5983d9c2c509c01d39c61fafb3307d90e1
barnden <brandon.nguyen1129@gmail.com>
2020-12-19 20:38:06 -0600

    update build script

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Live game: [generic.fish/clicker](https://generic.fish/clicker)
 ## Compiling (Windows)
 - Install npm/node, and run `npm i -g lessc minify`
 - Run `.\build.bat`
+- Serve the `dist` directory, e.g. `python -m http.server`
--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
+@echo off
+setlocal enableextensions
+
+if not exist ".\dist\assets\js\workers" md .\dist\assets\js\workers
+if not exist ".\dist\assets\images" md .\dist\assets\images
+
 call terser .\src\js\workers\clicker.js > .\dist\assets\js\workers\clicker.js
 call terser .\src\js\windows.js ^
             .\src\js\khoima.js ^
@@ -7,3 +13,4 @@ call lessc .\src\styles\clicker.less | call  minify --css > .\dist\assets\clicke
 call minify index.html > .\dist\index.html
 xcopy changelog.txt .\dist\changelog.txt /Y
 xcopy /s .\images .\dist\assets\images /Y
+endlocal
