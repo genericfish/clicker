44f46d57dd3e0b55de700a72362dadf12d64f8f0
barnden <brandon@barnden.dev>
2021-01-07 20:45:55 -0600

    drag on left mouse, drag&drop container

--- src/js/core/draggable.js
+++ src/js/core/draggable.js
@@ -1,7 +1,10 @@
 class Draggable {
     constructor (interactive, parent) {
-        if (!(interactive instanceof Element) || !(parent instanceof Element))
+        if (!(interactive instanceof Element))
             throw new Exception("[Drag] Expected HTML Element.")
+
+        // Assume entire element is draggable.
+        if (parent == undefined) parent = interactive
 
         this.previous = [0,0]
         this.draggable = interactive
@@ -16,7 +19,12 @@ class Draggable {
 
         this.draggable.addEventListener("mousedown", e => {
             e.preventDefault()
+
+            // only activate on lmb
+            if (e.buttons != 1) return
+
             this.previous = [e.clientX, e.clientY]
+            this.container.setAttribute("data-dragged", "data-dragged")
 
             if (this.hooks.mousedown.length)
                 for (let cb of this.hooks.mousedown)
@@ -27,6 +35,7 @@ class Draggable {
 
         document.addEventListener("mouseup", e => {
             document.removeEventListener("mousemove", drag_func)
+            this.container.removeAttribute("data-dragged")
 
             if (this.hooks.mouseup.length)
                 for (let cb of this.hooks.mouseup)
@@ -53,4 +62,49 @@ class Draggable {
     }
 
     add_hook(event, cb) { if (this.hooks.hasOwnProperty(event)) this.hooks[event].push(cb) }
+}
+
+class DropArea {
+    constructor (e) {
+        if (!(e instanceof Element))
+            throw new Exception("[DropArea] Expected HTML Element.")
+
+        this.area = e
+        this.whitelist = {
+            id: [],
+            class: [],
+            elements: []
+        }
+
+        let box = e.getBoundingClientRect()
+        this.bounds = [
+            box.left,
+            box.top,
+            box.left + box.width,
+            box.top + box.height
+        ]
+    }
+
+    add(v) {
+        if (typeof v == "string")
+            if (v.charAt(0) == '#')
+                this.whitelist.id.push(v.substr(1))
+            else
+                this.whitelist.class.push(v)
+        else if (v instanceof Element)
+            this.whitelist.elements.push(v)
+    }
+
+    in_whitelist(e) {
+        return this.whitelist.id.includes(e.id) ||
+                Array.from(e.classList).reduce(a,b => a || this.whitelist.class.includes(b)) ||
+                this.elements.includes(e)
+    }
+
+    in_bounds(x, y) {
+        return x >= this.bounds[0] &&
+                x <= this.bounds[2] &&
+                y >= this.bounds[1] &&
+                y <= this.bounds[3]
+    }
 }
